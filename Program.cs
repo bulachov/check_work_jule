3c42ad77d747a7087ac693e023c993983b030197
bulachov <bulachov@gmail.com>
2022-07-24 05:21:42 +0300

    работ анад программой в ветке прог

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿string[] GetData(string path)
+{
+    string s = File.ReadAllText(path);
+    string[] data = s.Split(' ');
+    return data;
+}
+
+string[] MainTask(string[] tosort, int howManySimbol)
+{
+    string[] result = new string[0];
+    for (int i = 0; i < tosort.Length; i++)
+    {
+        if (tosort[i].Length <= howManySimbol)
+        {
+            Array.Resize(ref result, result.Length + 1);
+            result[result.Length - 1] = tosort[i];
+        }
+
+    }
+}
+
+
+
