2637b193793d5d28c4aae92a1a332670433fe93c
bulachov <bulachov@gmail.com>
2022-07-24 05:54:36 +0300

    Тест программы пройдет

--- Program.cs
+++ Program.cs
@@ -5,19 +5,35 @@
     return data;
 }
 
-string[] MainTask(string[] tosort, int howManySimbol)
+string[] MainTask(string[] tosort, int Simbol)
 {
     string[] result = new string[0];
     for (int i = 0; i < tosort.Length; i++)
     {
-        if (tosort[i].Length <= howManySimbol)
+        if ((tosort[i].Length <= Simbol) && (tosort[i].Length >= 1))
         {
             Array.Resize(ref result, result.Length + 1);
             result[result.Length - 1] = tosort[i];
         }
 
     }
+    return result;
 }
 
+string Print(string[] array)
+{
+    string back = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        back += ($"array[{i + 1}]: {array[i]} ");
+    }
+    return back;
+}
 
+int howManySimbol = 3;
 
+string[] a = GetData("datatosort.txt");
+File.WriteAllText("indata.txt", Print(a));
+
+string[] b = MainTask(a, howManySimbol);
+File.WriteAllText("outdata.txt", Print(b));
